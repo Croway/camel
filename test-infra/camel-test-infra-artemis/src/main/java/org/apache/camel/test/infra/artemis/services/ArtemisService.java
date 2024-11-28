11b195e11024b4521510a24dfc31def6da86dde4
Croway <federico.mariani.1990@gmail.com>
2024-11-28 09:54:21 +0100

    Artemis

--- test-infra/camel-test-infra-artemis/src/main/java/org/apache/camel/test/infra/artemis/services/ArtemisService.java
+++ test-infra/camel-test-infra-artemis/src/main/java/org/apache/camel/test/infra/artemis/services/ArtemisService.java
@@ -18,21 +18,29 @@ package org.apache.camel.test.infra.artemis.services;
 
 import org.apache.activemq.artemis.core.server.QueueQueryResult;
 import org.apache.camel.test.infra.artemis.common.ArtemisProperties;
-import org.apache.camel.test.infra.common.services.TestService;
-import org.apache.camel.test.infra.common.services.TestServiceUtil;
-import org.junit.jupiter.api.extension.AfterAllCallback;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
+import org.apache.camel.test.infra.common.services.InfrastructureService;
 
-public interface ArtemisTestService extends AfterAllCallback, BeforeAllCallback, TestService, ArtemisService {
+public interface ArtemisService extends InfrastructureService {
 
-    @Override
-    default void afterAll(ExtensionContext extensionContext) throws Exception {
-        TestServiceUtil.tryShutdown(this, extensionContext);
+    String serviceAddress();
+
+    String userName();
+
+    String password();
+
+    int brokerPort();
+
+    default void registerProperties() {
+        // For compatibility with the previous format used by camel-sjms tests
+        System.setProperty(ArtemisProperties.SERVICE_ADDRESS, serviceAddress());
+        System.setProperty(ArtemisProperties.ARTEMIS_EXTERNAL, serviceAddress());
+        System.setProperty(ArtemisProperties.ARTEMIS_USERNAME, userName());
+        System.setProperty(ArtemisProperties.ARTEMIS_PASSWORD, password());
     }
 
-    @Override
-    default void beforeAll(ExtensionContext extensionContext) throws Exception {
-        TestServiceUtil.tryInitialize(this, extensionContext);
-    }
+    void restart();
+
+    long countMessages(String queue) throws Exception;
+
+    QueueQueryResult getQueueQueryResult(String queueQuery) throws Exception;
 }
